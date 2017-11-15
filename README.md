5c2267df28bd21c1bb0a17bc8f79f9fd6803fb85
Carlos Abdalla <abdalla@msdev.com.br>
2017-11-15 16:57:47 -0500

    player is done, but options, tests and so on still missing

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # react-triton-digital-player
-React component to use triton digital sdk
+React component to use [triton digital](https://www.tritondigital.com/) sdk
+
+Triton's technology is designed to help broadcasters, podcasters, and music services streamline their day-to-day operations, build their online audience, and maximize their online revenue.
+
+This component was develop to help the triton sdk implemantation into your react project.
+
+# prerequisites
+You have to import the Triton Digital SDK into you app.
+
+```
+<script src="//sdk.listenlive.co/web/2.9/td-sdk.min.js"></script>
+```
+
+# how to use
+
+#### *ES5*
+```
+var Player require('./containers/radio/player');
+```
+
+#### *ES6*
+```
+import Player from './containers/radio/player';
+```
+
+```
+<Player station="YOUR_STATION" options={options} />
+```
+
+*OBS:* **options are optionals.** 
+
+###EXAMPLE
+
+```
+import React, { Component } from 'react';
+import Player from './containers/radio/player';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">MY APP</h1>
+				</header>
+
+				<Player station="YOUR_STATION" options={options} />
+
+			</div>
+		);
+	}
+}
+
+export default App;
+```
+
+# options
+
+## *IN DEVELOPMENT*
---
+++ src/player.jsx
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+let player;
+
+class Player extends Component {
+	constructor(props, context) {
+		super(props, context);
+
+		this.initPlayerSDK = this.initPlayerSDK.bind(this);
+		this.onPlayerReady = this.onPlayerReady.bind(this);
+		this.onConfigurationError = this.onConfigurationError.bind(this);
+		this.onModuleError = this.onModuleError.bind(this);
+		this.onTrackCuePoint = this.onTrackCuePoint.bind(this);
+		this.onAdBlockerDetected = this.onAdBlockerDetected.bind(this);
+	}
+
+	componentWillMount() {
+		this.setState({ station: this.props.station });
+	}
+
+	componentDidMount() {
+		this.initPlayerSDK();
+	}
+
+	initPlayerSDK() {
+		//Player SDK is ready to be used, this is where you can instantiate a new TDSdk instance.
+		//Player configuration: list of modules
+		const tdPlayerConfig = {
+			coreModules: [
+				{
+					id: 'MediaPlayer',
+					playerId: 'td_container'
+				}
+			],
+			playerReady: this.onPlayerReady,
+			configurationError: this.onConfigurationError,
+			moduleError: this.onModuleError,
+			adBlockerDetected: this.onAdBlockerDetected
+		};
+
+		//Player instance
+		//eslint-disable-next-line
+		player = new TDSdk(tdPlayerConfig);
+	}
+
+	onPlayerReady() {
+		player.addEventListener('track-cue-point', this.onTrackCuePoint);
+		player.play({ station: this.state.station });
+	}
+
+	onConfigurationError(object) {
+		this.setState({
+			errorCode: object.data.errors[0].code,
+			errorMessage: object.data.errors[0].message
+		});
+	}
+
+	onModuleError(object) {
+		this.setState({
+			errorCode: object.data.errors[0].code,
+			errorMessage: object.data.errors[0].message
+		});
+	}
+
+	/* Callback function called to notify that a new Track CuePoint comes in. */
+	onTrackCuePoint(e) {
+		this.setState({
+			artistName: e.data.cuePoint.artistName,
+			musicTitle: e.data.cuePoint.cueTitle
+		});
+	}
+	/* Callback function called to notify that an Ad-Blocker was detected */
+	onAdBlockerDetected() {
+		console.log('AdBlockerDetected');
+	}
+
+	render() {
+		return (
+			<div>
+				<div id="td_container" />
+				<span>Artist Name:</span>
+				{this.state.artistName}
+				<br />
+
+				<span>Music Title:</span>
+				{this.state.musicTitle}
+				<div>
+					<button
+						onClick={e => {
+							e.preventDefault();
+							player.resume();
+						}}>
+						{' '}
+						>{' '}
+					</button>
+				</div>
+				<div>
+					<button
+						onClick={e => {
+							e.preventDefault();
+							player.pause();
+						}}>
+						{' '}
+						>||
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+Player.propTypes = {
+	station: PropTypes.string.isRequired,
+	options: PropTypes.object
+};
+
+export default Player;
